60e35563588b1754a67cf3737133adfe64145cc2
Łukasz Holeczek <lukasz@holeczek.pl>
2016-09-13 11:11:08 +0200

    Update Angular2 package.json & tsconfig

--- Angular2_Dev/karma.conf.js
+++ Angular2_Dev/karma.conf.js
@@ -1,7 +1,11 @@
 module.exports = function(config) {
 
-  var appBase   = 'app/';      // transpiled app JS files
-  var appAssets ='/base/app/'; // component assets fetched by Angular's compiler
+  var appBase    = 'app/';       // transpiled app JS and map files
+  var appSrcBase = 'app/';       // app source TS files
+  var appAssets  = '/base/app/'; // component assets fetched by Angular's compiler
+
+  var testBase    = 'testing/';       // transpiled test JS and map files
+  var testSrcBase = 'testing/';       // test source TS files
 
   config.set({
     basePath: '',
@@ -26,40 +30,48 @@ module.exports = function(config) {
 
       // Polyfills
       'node_modules/core-js/client/shim.js',
-
-      // Reflect and Zone.js
       'node_modules/reflect-metadata/Reflect.js',
+
+      // zone.js
       'node_modules/zone.js/dist/zone.js',
+      'node_modules/zone.js/dist/long-stack-trace-zone.js',
+      'node_modules/zone.js/dist/proxy.js',
+      'node_modules/zone.js/dist/sync-test.js',
+      'node_modules/zone.js/dist/jasmine-patch.js',
       'node_modules/zone.js/dist/async-test.js',
       'node_modules/zone.js/dist/fake-async-test.js',
-      'node_modules/zone.js/dist/sync-test.js',
-      'node_modules/zone.js/dist/proxy.js',
 
-      // RxJs.
+      // RxJs
       { pattern: 'node_modules/rxjs/**/*.js', included: false, watched: false },
       { pattern: 'node_modules/rxjs/**/*.js.map', included: false, watched: false },
 
-      // Angular 2 itself and the testing library
+      // Paths loaded via module imports:
+      // Angular itself
       {pattern: 'node_modules/@angular/**/*.js', included: false, watched: false},
       {pattern: 'node_modules/@angular/**/*.js.map', included: false, watched: false},
 
       {pattern: 'systemjs.config.js', included: false, watched: false},
+      {pattern: 'systemjs.config.extras.js', included: false, watched: false},
       'karma-test-shim.js',
 
       // transpiled application & spec code paths loaded via module imports
       {pattern: appBase + '**/*.js', included: false, watched: true},
+      {pattern: testBase + '**/*.js', included: false, watched: true},
 
-      // asset (HTML & CSS) paths loaded via Angular's component compiler
+
+      // Asset (HTML & CSS) paths loaded via Angular's component compiler
       // (these paths need to be rewritten, see proxies section)
       {pattern: appBase + '**/*.html', included: false, watched: true},
       {pattern: appBase + '**/*.css', included: false, watched: true},
 
-      // paths for debugging with source maps in dev tools
-      {pattern: appBase + '**/*.ts', included: false, watched: false},
-      {pattern: appBase + '**/*.js.map', included: false, watched: false}
+      // Paths for debugging with source maps in dev tools
+      {pattern: appSrcBase + '**/*.ts', included: false, watched: false},
+      {pattern: appBase + '**/*.js.map', included: false, watched: false},
+      {pattern: testSrcBase + '**/*.ts', included: false, watched: false},
+      {pattern: testBase + '**/*.js.map', included: false, watched: false}
     ],
 
-    // proxied base paths for loading assets
+    // Proxied base paths for loading assets
     proxies: {
       // required for component assets fetched by Angular's compiler
       "/app/": appAssets
