f250f9b4c0cf3a5990a81f40315174799cce7e9f
Łukasz Holeczek <lukasz@holeczek.pl>
2016-09-14 12:35:44 +0200

    add Angular2_Demo

--- demo/Angular2_Demo/app/app.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-import { NAV_DROPDOWN_DIRECTIVES }  from './shared/nav-dropdown.directive';
-
-@Component({
-    selector: 'body',
-    template: '<router-outlet></router-outlet>',
-    directives: [ NAV_DROPDOWN_DIRECTIVES ]
-})
-export class AppComponent { }
