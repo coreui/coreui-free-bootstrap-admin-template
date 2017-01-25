ef2262867e786915d7414185aa28dbc49f719ec6
Łukasz Holeczek <lukasz@holeczek.pl>
2017-01-25 11:21:06 +0100

    clean

--- React_Full_Project/src/components/NavLink/NavLink.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { Link } from 'react-router'
-
-export default React.createClass({
-  render() {
-    return <Link {...this.props} activeClassName="active"/>
-  }
-})
