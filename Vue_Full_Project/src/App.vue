aebafc8793b901d5607e4b9e79403f0800f72cef
Łukasz Holeczek <lukasz@holeczek.pl>
2017-08-10 13:38:19 +0200

    update

--- Vue_Full_Project/src/App.vue
+++ Vue_Full_Project/src/App.vue
@@ -7,3 +7,16 @@ export default {
   name: 'app'
 }
 </script>
+
+<style>
+  // Import Font Awesome Icons Set
+  $fa-font-path: "~font-awesome/fonts/";
+  @import "~font-awesome/css/font-awesome.min.css";
+  // Import Simple Line Icons Set
+  $simple-line-font-path: "~simple-line-icons/fonts/";
+  @import "~simple-line-icons/css/simple-line-icons.css";
+</style>
+<style lang="scss">
+  // Import Main styles for this application
+  @import "./scss/style";
+</style>
--- Vue_Starter/src/App.vue
+++ Vue_Starter/src/App.vue
@@ -7,14 +7,17 @@ export default {
   name: 'app'
 }
 </script>
-<!--
+
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style> -->
+  // Import Font Awesome Icons Set
+  $fa-font-path: "~font-awesome/fonts/";
+  @import "~font-awesome/css/font-awesome.min.css";
+  // Import Simple Line Icons Set
+  $simple-line-font-path: "~simple-line-icons/fonts/";
+  @import "~simple-line-icons/css/simple-line-icons.css";
+</style>
+<style lang="scss">
+  // Import Main styles for this application
+  @import "./scss/style";
+</style>
+
