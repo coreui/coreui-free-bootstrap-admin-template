4ca5b0ca8cd7288f4c2af8c32a2d06f9b41e9383
Łukasz Holeczek <lukasz@holeczek.pl>
2017-08-10 13:45:28 +0200

    Data driven sidebar component. Close #219

--- React_Full_Project/src/components/Sidebar/Sidebar.js
+++ React_Full_Project/src/components/Sidebar/Sidebar.js
@@ -1,5 +1,8 @@
-import React, { Component } from 'react';
-import { NavLink } from 'react-router-dom'
+import React, {Component} from 'react';
+import {NavLink} from 'react-router-dom';
+import {Badge, Nav, NavItem} from 'reactstrap';
+import classNames from 'classnames';
+import nav from './_nav'
 
 class Sidebar extends Component {
 
@@ -8,93 +11,86 @@ class Sidebar extends Component {
     e.target.parentElement.classList.toggle('open');
   }
 
-  activeRoute(routeName) {
-    return this.props.location.pathname.indexOf(routeName) > -1 ? 'nav-item nav-dropdown open' : 'nav-item nav-dropdown';
+  activeRoute(routeName, props) {
+    // return this.props.location.pathname.indexOf(routeName) > -1 ? 'nav-item nav-dropdown open' : 'nav-item nav-dropdown';
+    return props.location.pathname.indexOf(routeName) > -1 ? 'nav-item nav-dropdown open' : 'nav-item nav-dropdown';
+
   }
 
+  // todo Sidebar nav secondLevel
   // secondLevelActive(routeName) {
   //   return this.props.location.pathname.indexOf(routeName) > -1 ? "nav nav-second-level collapse in" : "nav nav-second-level collapse";
   // }
 
+
   render() {
+
+    const props = this.props;
+    const activeRoute = this.activeRoute;
+    const handleClick = this.handleClick;
+
+    // badge addon to NavItem
+    const badge = (badge) => {
+      if (badge) {
+        const classes = classNames( badge.class );
+        return (<Badge className={ classes } color={ badge.variant }>{ badge.text }</Badge>)
+      }
+    };
+
+    // simple wrapper for nav-title item
+    const wrapper = item => { return (!item.wrapper ? item.name : (React.createElement(item.wrapper.element, item.wrapper.attributes, item.name))) };
+
+    // nav list section title
+    const title =  (title, key) => {
+      const classes = classNames( "nav-title", title.class);
+      return (<li key={key} className={ classes }>{wrapper(title)} </li>);
+    };
+
+    // nav list divider
+    const divider = (divider, key) => (<li key={key} className="divider"></li>);
+
+    // nav item with nav link
+    const navItem = (item, key) => {
+      const classes = classNames( "nav-link", item.class);
+      return (
+        <NavItem key={key}>
+          <NavLink to={item.url} className={ classes } activeClassName="active">
+            <i className={item.icon}></i>{item.name}{badge(item.badge)}
+          </NavLink>
+        </NavItem>
+      )
+    };
+
+    // nav dropdown
+    const navDropdown = (item, key) => {
+      return (
+        <li key={key} className={activeRoute(item.url, props)}>
+          <a className="nav-link nav-dropdown-toggle" href="#" onClick={handleClick.bind(this)}><i className={item.icon}></i> {item.name}</a>
+          <ul className="nav-dropdown-items">
+            {navList(item.children)}
+          </ul>
+        </li>)
+    };
+
+    // nav link
+    const navLink = (item, idx) =>
+      item.title ? title(item, idx) :
+      item.divider ? divider(item, idx) :
+      item.children ? navDropdown(item, idx)
+                    : navItem(item, idx) ;
+
+    // nav list
+    const navList = (items) => {
+      return items.map( (item, index) => navLink(item, index) );
+    };
+
+    // sidebar-nav root
     return (
       <div className="sidebar">
         <nav className="sidebar-nav">
-          <ul className="nav">
-            <li className="nav-item">
-              <NavLink to={'/dashboard'} className="nav-link" activeClassName="active"><i className="icon-speedometer"></i> Dashboard <span className="badge badge-info">NEW</span></NavLink>
-            </li>
-            <li className="nav-title">
-              UI Elements
-            </li>
-            <li className={this.activeRoute("/components")}>
-              <a className="nav-link nav-dropdown-toggle" href="#" onClick={this.handleClick.bind(this)}><i className="icon-puzzle"></i> Components</a>
-              <ul className="nav-dropdown-items">
-                <li className="nav-item">
-                  <NavLink to={'/components/buttons'} className="nav-link" activeClassName="active"><i className="icon-puzzle"></i> Buttons</NavLink>
-                </li>
-                <li className="nav-item">
-                  <NavLink to={'/components/social-buttons'} className="nav-link" activeClassName="active"><i className="icon-puzzle"></i> Social Buttons</NavLink>
-                </li>
-                <li className="nav-item">
-                  <NavLink to={'/components/cards'} className="nav-link" activeClassName="active"><i className="icon-puzzle"></i> Cards</NavLink>
-                </li>
-                <li className="nav-item">
-                  <NavLink to={'/components/forms'} className="nav-link" activeClassName="active"><i className="icon-puzzle"></i> Forms</NavLink>
-                </li>
-                <li className="nav-item">
-                  <NavLink to={'/components/modals'} className="nav-link" activeClassName="active"><i className="icon-puzzle"></i> Modals</NavLink>
-                </li>
-                <li className="nav-item">
-                  <NavLink to={'/components/switches'} className="nav-link" activeClassName="active"><i className="icon-puzzle"></i> Switches</NavLink>
-                </li>
-                <li className="nav-item">
-                  <NavLink to={'/components/tables'} className="nav-link" activeClassName="active"><i className="icon-puzzle"></i> Tables</NavLink>
-                </li>
-                <li className="nav-item">
-                  <NavLink to={'/components/tabs'} className="nav-link" activeClassName="active"><i className="icon-puzzle"></i> Tabs</NavLink>
-                </li>
-              </ul>
-            </li>
-            <li className={this.activeRoute("/icons")}>
-              <a className="nav-link nav-dropdown-toggle" href="#" onClick={this.handleClick.bind(this)}><i className="icon-star"></i> Icons</a>
-              <ul className="nav-dropdown-items">
-                <li className="nav-item">
-                  <NavLink to={'/icons/font-awesome'} className="nav-link" activeClassName="active"><i className="icon-star"></i> Font Awesome</NavLink>
-                </li>
-                <li className="nav-item">
-                  <NavLink to={'/icons/simple-line-icons'} className="nav-link" activeClassName="active"><i className="icon-star"></i> Simple Line Icons</NavLink>
-                </li>
-              </ul>
-            </li>
-            <li className="nav-item">
-              <NavLink to={'/widgets'} className="nav-link" activeClassName="active"><i className="icon-calculator"></i> Widgets <span className="badge badge-info">NEW</span></NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink to={'/charts'} className="nav-link" activeClassName="active"><i className="icon-pie-chart"></i> Charts</NavLink>
-            </li>
-            <li className="divider"></li>
-            <li className="nav-title">
-              Extras
-            </li>
-            <li className="nav-item nav-dropdown">
-              <a className="nav-link nav-dropdown-toggle" href="#" onClick={this.handleClick.bind(this)}><i className="icon-star"></i> Pages</a>
-              <ul className="nav-dropdown-items">
-                <li className="nav-item">
-                  <NavLink to={'/login'} className="nav-link" activeClassName="active"><i className="icon-star"></i> Login</NavLink>
-                </li>
-                <li className="nav-item">
-                  <NavLink to={'/register'} className="nav-link" activeClassName="active"><i className="icon-star"></i> Register</NavLink>
-                </li>
-                <li className="nav-item">
-                  <NavLink to={'/404'} className="nav-link" activeClassName="active"><i className="icon-star"></i> Error 404</NavLink>
-                </li>
-                <li className="nav-item">
-                  <NavLink to={'/500'} className="nav-link" activeClassName="active"><i className="icon-star"></i> Error 500</NavLink>
-                </li>
-              </ul>
-            </li>
-          </ul>
+          <Nav>
+            {navList(nav.items)}
+          </Nav>
         </nav>
       </div>
     )
---
+++ React_Starter/src/components/Sidebar/_nav.js
@@ -0,0 +1,13 @@
+export default {
+  items: [
+    {
+      name: 'Dashboard',
+      url: '/dashboard',
+      icon: 'icon-speedometer',
+      badge: {
+        variant: 'info',
+        text: 'NEW'
+      }
+    }
+  ]
+};
