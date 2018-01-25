ab63789d35ce81b273883e1a0a6e5221b4031883
xidedix <xidedix@users.noreply.github.com>
2018-01-25 12:05:19 +0100

    v1.0.9 AngularJS demo

---
+++ demo/AngularJS_Demo/js/routes.js
@@ -0,0 +1,124 @@
+angular
+.module('app')
+.config(['$stateProvider', '$urlRouterProvider', '$ocLazyLoadProvider', '$breadcrumbProvider', function($stateProvider, $urlRouterProvider, $ocLazyLoadProvider, $breadcrumbProvider) {
+
+  $urlRouterProvider.otherwise('/dashboard');
+
+  $ocLazyLoadProvider.config({
+    // Set to true if you want to see what and when is dynamically loaded
+    debug: true
+  });
+
+  $breadcrumbProvider.setOptions({
+    prefixStateName: 'app.main',
+    includeAbstract: true,
+    template: '<li class="breadcrumb-item" ng-repeat="step in steps" ng-class="{active: $last}" ng-switch="$last || !!step.abstract"><a ng-switch-when="false" href="{{step.ncyBreadcrumbLink}}">{{step.ncyBreadcrumbLabel}}</a><span ng-switch-when="true">{{step.ncyBreadcrumbLabel}}</span></li>'
+  });
+
+  $stateProvider
+  .state('app', {
+    abstract: true,
+    templateUrl: 'views/common/layouts/full.html',
+    //page title goes here
+    ncyBreadcrumb: {
+      label: 'Root',
+      skip: true
+    },
+    resolve: {
+      loadCSS: ['$ocLazyLoad', function($ocLazyLoad) {
+        // you can lazy load CSS files
+        return $ocLazyLoad.load([{
+          serie: true,
+          name: 'Flags',
+          files: ['vendors/css/flag-icon.min.css']
+        },{
+          serie: true,
+          name: 'Font Awesome',
+          files: ['vendors/css/font-awesome.min.css']
+        },{
+          serie: true,
+          name: 'Simple Line Icons',
+          files: ['vendors/css/simple-line-icons.min.css']
+        }]);
+      }],
+      loadPlugin: ['$ocLazyLoad', function ($ocLazyLoad) {
+        // you can lazy load files for an existing module
+        return $ocLazyLoad.load([{
+          serie: true,
+          name: 'chart.js',
+          files: [
+            'vendors/js/Chart.min.js',
+            'vendors/js/angular-chart.min.js'
+          ]
+        }]);
+      }],
+    }
+  })
+  .state('app.main', {
+    url: '/dashboard',
+    templateUrl: 'views/main.html',
+    //page title goes here
+    ncyBreadcrumb: {
+      label: 'Home',
+    },
+    //page subtitle goes here
+    params: { subtitle: 'Welcome to ROOT powerfull Bootstrap & AngularJS UI Kit' },
+    resolve: {
+      loadPlugin: ['$ocLazyLoad', function ($ocLazyLoad) {
+        // you can lazy load files for an existing module
+        return $ocLazyLoad.load([
+          {
+            serie: true,
+            name: 'chart.js',
+            files: [
+              'vendors/js/Chart.min.js',
+              'vendors/js/angular-chart.min.js'
+            ]
+          },
+        ]);
+      }],
+      loadMyCtrl: ['$ocLazyLoad', function($ocLazyLoad) {
+        // you can lazy load controllers
+        return $ocLazyLoad.load({
+          files: ['js/controllers/main.js']
+        });
+      }]
+    }
+  })
+  .state('appSimple', {
+    abstract: true,
+    templateUrl: 'views/common/layouts/simple.html',
+    resolve: {
+      loadCSS: ['$ocLazyLoad', function($ocLazyLoad) {
+        // you can lazy load CSS files
+        return $ocLazyLoad.load([{
+          serie: true,
+          name: 'Font Awesome',
+          files: ['vendors/css/font-awesome.min.css']
+        },{
+          serie: true,
+          name: 'Simple Line Icons',
+          files: ['vendors/css/simple-line-icons.min.css']
+        }]);
+      }],
+    }
+  })
+
+  // Additional Pages
+  .state('appSimple.login', {
+    url: '/login',
+    templateUrl: 'views/pages/login.html'
+  })
+  .state('appSimple.register', {
+    url: '/register',
+    templateUrl: 'views/pages/register.html'
+  })
+  .state('appSimple.404', {
+    url: '/404',
+    templateUrl: 'views/pages/404.html'
+  })
+  .state('appSimple.500', {
+    url: '/500',
+    templateUrl: 'views/pages/500.html'
+  })
+}]);
---
+++ demo/AngularJS_Demo/views/common/layouts/simple.html
@@ -0,0 +1,1 @@
+<ui-view></ui-view>
