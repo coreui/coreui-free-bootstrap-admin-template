f5a5231aa3f562e0f024db1bbb10412e07d998d1
Miguel Veloso <mvelosop@gmail.com>
2018-04-16 00:11:31 +0100

    Implemented getDistributionReferences and getDistributionDocument

--- build/vendors-aspnetcore.js
+++ build/vendors-aspnetcore.js
@@ -2,10 +2,12 @@
 
 'use strict'
 
-const getVendorReferences = (stringData) => {
-  // from https://stackoverflow.com/questions/171480/regex-grabbing-values-between-quotation-marks
-  const regEx = /(["'])node_modules(?:(?=(\\?))\2.)*?\1/ig;
+const distributionRoot = 'lib';
 
+// from https://stackoverflow.com/questions/171480/regex-grabbing-values-between-quotation-marks
+const regEx = /(["'])node_modules(?:(?=(\\?))\2.)*?\1/ig;
+
+const getVendorReferences = (stringData) => {
   if (stringData.search(regEx) === -1) {
     return [];
   }
@@ -20,6 +22,43 @@ const getVendorReferences = (stringData) => {
   return references;
 };
 
+const getDistributionReferences = (vendorReferences) => {
+  let distReferences = [];
+
+  vendorReferences.forEach(reference => {
+    distReferences.push(reference.replace(/node_modules/i, distributionRoot));
+  });
+
+  return distReferences;
+};
+
+
+const getDistributionDocument = (document) => {
+  let vendorReferences = getVendorReferences(document);
+  let distributionReferences = getDistributionReferences(vendorReferences);
+
+  const replaceReference = (match) => {
+    let search = match.substr(1, match.length - 2);
+    let index = vendorReferences.indexOf(search);
+
+    if (index === -1) {
+      return match;
+    }
+
+    return `"~/${distributionReferences[index]}"`;
+  };
+
+  let preparedDocument = document.replace(regEx, replaceReference)
+
+  return {
+    distributionDocument: preparedDocument,
+    sourceReferences: vendorReferences,
+    destinationReferences: distributionReferences
+  };
+};
+
 module.exports = {
-  getVendorReferences
+  getVendorReferences,
+  getDistributionReferences,
+  getDistributionDocument
 }
--- test/build/vendors-aspnetcore.test.js
+++ test/build/vendors-aspnetcore.test.js
@@ -1,7 +1,7 @@
 const should = require('should');
 const vendors = require('../../build/vendors-aspnetcore');
 
-const vendorReferenceData = `<!DOCTYPE html>
+const originalDocument = `<!DOCTYPE html>
 <html lang="en">
   <head>
     <meta charset="utf-8">
@@ -40,8 +40,83 @@ const vendorReferenceList = [
   'node_modules/perfect-scrollbar/dist/perfect-scrollbar.min.js',
   'node_modules/@coreui/coreui/dist/js/coreui.min.js'];
 
+const distributionReferenceList = [
+  'lib/flag-icon-css/css/flag-icon.min.css',
+  'lib/font-awesome/css/font-awesome.min.css',
+  'lib/simple-line-icons/css/simple-line-icons.css',
+  'lib/jquery/dist/jquery.min.js',
+  'lib/popper.js/dist/umd/popper.min.js',
+  'lib/bootstrap/dist/js/bootstrap.min.js',
+  'lib/pace-progress/pace.min.js',
+  'lib/perfect-scrollbar/dist/perfect-scrollbar.min.js',
+  'lib/@coreui/coreui/dist/js/coreui.min.js'];
+
+const distributionDocument = `<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <meta name="description" content="CoreUI - Open Source Bootstrap Admin Template">
+    <meta name="author" content="Łukasz Holeczek">
+    <meta name="keyword" content="Bootstrap,Admin,Template,Open,Source,jQuery,CSS,HTML,RWD,Dashboard">
+    <title>CoreUI Free Bootstrap Admin Template</title>
+    <!-- Icons-->
+    <link href="~/lib/flag-icon-css/css/flag-icon.min.css" rel="stylesheet">
+    <link href="~/lib/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+    <link href="~/lib/simple-line-icons/css/simple-line-icons.css" rel="stylesheet">
+    <!-- Main styles for this application-->
+    <link href="css/style.css" rel="stylesheet">
+  </head>
+  <body class="app header-fixed sidebar-fixed aside-menu-fixed sidebar-lg-show">
+    <!-- Bootstrap and necessary plugins-->
+    <script src="~/lib/jquery/dist/jquery.min.js"></script>
+    <script src="~/lib/popper.js/dist/umd/popper.min.js"></script>
+    <script src="~/lib/bootstrap/dist/js/bootstrap.min.js"></script>
+    <script src="~/lib/pace-progress/pace.min.js"></script>
+    <script src="~/lib/perfect-scrollbar/dist/perfect-scrollbar.min.js"></script>
+    <script src="~/lib/@coreui/coreui/dist/js/coreui.min.js"></script>
+  </body>
+</html>`;
+
 describe('getVendorReferences', () => {
-  it('should return vendor reference list', () => {
-    vendors.getVendorReferences(vendorReferenceData).should.be.eql(vendorReferenceList);
-    })
+  it('Should return an empty list when references don\'t exist', () => {
+    vendors.getVendorReferences('').should.be.eql([]);
+  });
+
+  it('Should return vendor reference list when references exist', () => {
+    vendors.getVendorReferences(originalDocument).should.be.eql(vendorReferenceList);
+  });
+});
+
+describe('getDistributionReference', () => {
+  const vendorReferenceList = [
+    'node_modules/flag-icon-css/css/flag-icon.min.css',
+    'node_modules/font-awesome/css/font-awesome.min.css',
+    'NODE_MODULES/jquery/dist/jquery.min.js',
+    'node_modules/popper.js/NODE_MODULES/dist/umd/popper.min.js',
+    'NODE_MODULES/bootstrap/node_modules/img/image1.jpg',
+    'node_modules/pace-progress/sound.mp3'];
+
+  const distributionReferenceList = [
+    'lib/flag-icon-css/css/flag-icon.min.css',
+    'lib/font-awesome/css/font-awesome.min.css',
+    'lib/jquery/dist/jquery.min.js',
+    'lib/popper.js/NODE_MODULES/dist/umd/popper.min.js',
+    'lib/bootstrap/node_modules/img/image1.jpg',
+    'lib/pace-progress/sound.mp3'];
+
+    it('Should replace initial node_modules with distribution root', () => {
+      vendors.getDistributionReferences(vendorReferenceList).should.be.eql(distributionReferenceList);
+  })
+});
+
+describe('getDistributionDocument', () => {
+  it('Should change node_modules references to distribution root', () => {
+    let result = vendors.getDistributionDocument(originalDocument);
+
+    result.distributionDocument.should.be.equal(distributionDocument);
+    result.sourceReferences.should.be.eql(vendorReferenceList);
+    result.destinationReferences.should.be.eql(distributionReferenceList);
+  });
 });
