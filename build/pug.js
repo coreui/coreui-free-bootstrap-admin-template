53ac2ee9ad53c79f66aa1f90dced34a8d517382b
Łukasz Holeczek <lukasz@holeczek.pl>
2018-06-23 10:02:44 +0200

    feat: add pug support

--- build/pug.js
+++ build/pug.js
@@ -2,16 +2,14 @@
 
 'use strict'
 
-// const args = process.argv.slice(2)
+const args = process.argv.slice(2)
 
 const fs         = require('fs')
 const path       = require('path')
 const mkdirp     = require('mkdirp')
 const pug        = require('pug')
-// const src        = './pug/'
+const src        = './pug/'
 const dest       = './src/'
-// const views      = src + 'views/'
-// const pages      = src + 'pages/'
 const pkg        = require(path.resolve(__dirname, '../package.json'))
 const beautify   = require('js-beautify').html
 const jsbOptions = {
@@ -21,6 +19,7 @@ const jsbOptions = {
   content_unformatted: ['textarea'],
   extra_liners: ['']
 }
+const version    = args[0];
 
 const basename   = path.basename
 const dirname    = path.dirname
@@ -47,14 +46,13 @@ const compile = (filename, basedir) => {
   const levels = filename.replace('pug/views/', '').replace('pug/pages/', '').split('/').length
   const base = (levels) => {
     let path = './'
-
     while (levels > 1) {
       levels = levels - 1;
       path = path + '../'
     }
     return path
   }
-  // console.log()
+
   const fn = pug.compileFile(filename, {
     basedir: basedir,
     pretty: true,
@@ -65,16 +63,30 @@ const compile = (filename, basedir) => {
   return html
 }
 
-// Build base version
-const buildBaseVersion = () => {
+// Build html files
+const compileHtml = () => {
+  // Build index
+  if (version === 'ajax') {
+    const html = compile('./pug/layout/index.pug', './pug/layout/')
+    fs.writeFile(resolve(dest, 'index.html'), beautify(html, jsbOptions), function(err) {
+      if(err) {
+        return console.log(err);
+      }
+      console.log('index.html file was saved!');
+    })
+  }
 
   // Build views
   const views = walkSync('./pug/views/')
   views.forEach((view) => {
     if (isPug(view)) {
       const html = compile(view, './pug/layout/')
-      const file = view.replace('pug/views/', '').replace('.pug', '.html')
-
+      let file
+      if (version === 'ajax') {
+        file = view.replace('pug/', '').replace('.pug', '.html')
+      } else {
+        file = view.replace('pug/views/', '').replace('.pug', '.html')
+      }
       // Create tree
       mkdirp.sync(resolve(dest, dirname(file)))
       // Create HTML file
@@ -82,18 +94,16 @@ const buildBaseVersion = () => {
         if(err) {
           return console.log(err)
         }
-        console.log(file + " file was saved!")
+        console.log(file + ' file was saved!')
       })
     }
   })
-
   // Build pages
   const pages = walkSync('./pug/pages')
   pages.forEach((page) => {
     if (isPug(page)) {
       const html = compile(page, './pug/layout/')
       const file = page.replace('pug/pages/', '').replace('.pug', '.html')
-
       // Create tree
       mkdirp.sync(resolve(dest, dirname(file)))
       // Create HTML file
@@ -101,10 +111,10 @@ const buildBaseVersion = () => {
         if(err) {
           return console.log(err)
         }
-        console.log(file + " file was saved!")
+        console.log(file + ' file was saved!')
       })
     }
   })
 }
 
-buildBaseVersion()
+compileHtml()
