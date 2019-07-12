e326136bdabca355d5382ad781d3bd9d0bc608f3
xidedix <xidedix@users.noreply.github.com>
2019-07-12 12:50:52 +0200

    Ship: v2.1.15

--- src/js/src/popovers.js
+++ src/js/src/popovers.js
@@ -2,7 +2,7 @@ import $ from 'jquery'
 
 /**
  * --------------------------------------------------------------------------
- * CoreUI Free Boostrap Admin Template (v2.1.14): popovers.js
+ * CoreUI Free Boostrap Admin Template (v2.1.15): popovers.js
  * Licensed under MIT (https://coreui.io/license)
  * --------------------------------------------------------------------------
  */
--- src/js/src/tooltips.js
+++ src/js/src/tooltips.js
@@ -2,7 +2,7 @@ import $ from 'jquery'
 
 /**
  * --------------------------------------------------------------------------
- * CoreUI Free Boostrap Admin Template (v2.1.14): tooltips.js
+ * CoreUI Free Boostrap Admin Template (v2.1.15): tooltips.js
  * Licensed under MIT (https://coreui.io/license)
  * --------------------------------------------------------------------------
  */
