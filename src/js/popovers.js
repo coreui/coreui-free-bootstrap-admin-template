d99f856a760f069541ec41b7424509ffa97359a5
Łukasz Holeczek <lukasz@holeczek.pl>
2021-03-15 15:48:36 +0100

    fix: change wrong data attribute name `[data-toggle="popover"]` to `[data-coreui-toggle="popover"]`

--- src/js/popovers.js
+++ src/js/popovers.js
@@ -7,12 +7,8 @@
  * --------------------------------------------------------------------------
  */
 
-document.querySelectorAll('[data-toggle="popover"]').forEach(element => {
+document.querySelectorAll('[data-coreui-toggle="popover"]').forEach(element => {
   // eslint-disable-next-line no-new
   new coreui.Popover(element)
 })
 
-// $('[data-toggle="popover"]').popover()
-// $('.popover-dismiss').popover({
-//   trigger: 'focus'
-// })
