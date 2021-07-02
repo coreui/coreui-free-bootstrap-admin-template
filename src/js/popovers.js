0d29065e2199c1b139b38b7d8bb8b74607631111
Łukasz Holeczek <lukasz@holeczek.pl>
2021-07-02 17:30:43 +0200

    release: v4.0.0

--- src/js/popovers.js
+++ src/js/popovers.js
@@ -2,7 +2,7 @@
 
 /**
  * --------------------------------------------------------------------------
- * CoreUI Free Boostrap Admin Template (v4.0.0-beta.2): popovers.js
+ * CoreUI Free Boostrap Admin Template (v4.0.0): popovers.js
  * Licensed under MIT (https://coreui.io/license)
  * --------------------------------------------------------------------------
  */
